5879d52487c594434d758da0d6acf408ecb71491
Martin Lawrence <marsixsoft@yahoo.com>
2017-05-31 04:20:08 +0300

    New carousel!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,14 +5,16 @@
 *= require turbolinks
 *= require modernizr
 *= require ./mdl/main
-*  require ./materialize/global
+*= require ./materialize/global
 *= require ./materialize/carousel
 *= require ./materialize/collapsible
 *= require ./materialize/dropdown
+*  require ./materialize.min
 *= require_tree ./snippets
 */
 
-//function afterDOMinit(ev) { function() }
-document.addEventListener('turbolinks:load', function() { componentHandler.upgradeDom() });
+function someElemsInit() { $('.carousel.carousel-slider').carousel({fullWidth: true}); }
+
+$(document).ready(someElemsInit)
+document.addEventListener('turbolinks:load', function() { componentHandler.upgradeDom(); someElemsInit() });
 $.turbo.use('turbolinks:load', 'turbolinks:request-start');
-//document.addEventListener('DOMContentLoaded', afterDOMinit, false);
