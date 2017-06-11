94dce1dacbb031f7abb847165fbea900dd852a19
Martin Lawrence <marsixsoft@yahoo.com>
2017-06-11 13:30:51 +0300

    Enabled css uglifying in production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,25 +1,18 @@
-/*
-*= require jquery
-*= require jquery.turbolinks
-*= require rails-ujs
-*  require modernizr
-*= require jquery.slick
-*  require jquery.mousewheel
-*= require ./mdl/main
-*= require ./materialize/jquery.easing.1.3
-*= require ./materialize/collapsible
-*= require_tree ./snippets
-*= require turbolinks
-*/
+//= require jquery
+//= require jquery.turbolinks
+//= require rails-ujs
+//= require jquery.slick
+//  require jquery.mousewheel
+//= require ./mdl/main
+//= require ./materialize/jquery.easing.1.3
+//= require ./materialize/collapsible
+//= require_tree ./snippets
+//= require turbolinks
 
 function someElemsInit(){
 	componentHandler.upgradeDom()
-	$('.collapsible').collapsible()
+	//$('.collapsible').collapsible()
 	$('#slider').slick({ dots: true, prevArrow: false, nextArrow: false })
-	itemSliderInit()
 }
 
-$(document).ready(someElemsInit)
 document.addEventListener('turbolinks:load', someElemsInit)
-//document.addEventListener('turbolinks:before-cache', someElemsInit)
-//$.turbo.use('turbolinks:load', 'turbolinks:request-start');
--- app/assets/javascripts/snippets/itemSliderInit.js
+++ app/assets/javascripts/snippets/itemSliderInit.js
@@ -1,34 +1,39 @@
-function itemSliderInit(){
-	$('.itemSlider').slick({
-		dots: true,
-    prevArrow: false,
-    nextArrow: false,
-		speed: 400,
-		slidesToShow: 4,
-		slidesToScroll: 4,
-		responsive: [
-			{
-				breakpoint: 1200,
-				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 3,
-					infinite: true,
-				}
-			},
-			{
-				breakpoint: 934,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 2
-				}
-			},
-			{
-				breakpoint: 640,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1
-				}
-			}
-		]
-	})
-}
+document.addEventListener('turbolinks:load',
+	function (){
+		function bindSlider(){
+			$('.itemSlider').slick({
+				dots: true,
+		    prevArrow: false,
+		    nextArrow: false,
+				speed: 400,
+				slidesToShow: 4,
+				slidesToScroll: 4,
+				responsive: [
+					{
+						breakpoint: 1200,
+						settings: {
+							slidesToShow: 3,
+							slidesToScroll: 3,
+							infinite: true,
+						}
+					},
+					{
+						breakpoint: 934,
+						settings: {
+							slidesToShow: 2,
+							slidesToScroll: 2
+						}
+					},
+					{
+						breakpoint: 640,
+						settings: {
+							slidesToShow: 1,
+							slidesToScroll: 1
+						}
+					}
+				]
+			})
+		}
+		setTimeout(bindSlider, 1)
+	}
+)
