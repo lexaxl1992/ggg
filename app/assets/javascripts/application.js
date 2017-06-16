f9e66f783d63951744b7a166ec1befe50c80777b
Martin Lawrence <marsixsoft@yahoo.com>
2017-06-16 09:13:49 +0300

    Small reorganization of assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,7 +4,6 @@
 //= require jquery.slick
 //  require jquery.mousewheel
 //= require ./mdl/main
-//= require ./owl/min
 //= require ./materialize/jquery.easing.1.3
 //= require ./materialize/collapsible
 //= require_tree ./snippets
