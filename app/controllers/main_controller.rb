d7dcb8c0a25b9f87ba943a85f2ad8d82563744ed
Martin Lawrence <marsixsoft@yahoo.com>
2017-07-01 01:33:05 +0300

    Reworked Model level, started backend developing

--- app/controllers/main_controller.rb
+++
@@ -1,9 +0,0 @@
-class MainController < ApplicationController
-	def index
-		render 'index'
-	end
-
-	def about
-		render 'about'
-	end
-end
--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-	root 'main#index'
-	get '/about', to: 'main#about'
-	get '/adminpanel', to: 'adminpanel#index'
-	resources :items, controller: 'adminpanel'
-end
