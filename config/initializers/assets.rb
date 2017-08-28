f9d1f7508b2c2558bfd1c78ace08c426b78f5969
Martin Lawrence <marsixsoft@yahoo.com>
2017-08-29 01:20:45 +0300

    Fixed tinymce precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,11 +3,11 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( * )
+Rails.application.config.assets.precompile << "tinymce-jquery.js"
 Rails.root.join('vendor', 'assets')
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-#Rails.application.config.assets.precompile += %w( fonts/* )
+# Rails.application.config.assets.precompile += %w( fonts/* )
