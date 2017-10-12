b39c2ba33c31561f2130466398b165cb335db343
Martin Lawrence <marsixsoft@yahoo.com>
2017-10-12 15:21:03 +0300

    added mibile gallery

--- app/assets/javascripts/initializers/gallery.js
+++ app/assets/javascripts/initializers/gallery.js
@@ -1,31 +1,30 @@
-// jshint esversion: 6
-
 function bind_gallery() {
-    let options = {
+    'use strict';
+
+    var options = {
         "history": false
     };
 
-    let windowID, pswpElement = document.querySelectorAll('.pswp')[0], gallery_items;
-    let slideDownloadButton = new Vue({
+    var windowID, pswpElement = document.querySelectorAll('.pswp')[0], gallery_items;
+    var slideDownloadButton = new Vue({
         el: '#download-slide',
         data: {
             slideURL: 'none'
         }
     });
 
-    document.addEventListener('click', (event) => {
+    document.addEventListener('click', function (event) {
         if (event.target.classList.contains('open--gallery')) {
-            let itemID = event.target.dataset.itemId;
+            var itemID = event.target.dataset.itemId;
             windowID = 'show_item_' + itemID;
-            gallery_items = JSON.parse(document.getElementById('slider_' + itemID).dataset.json);
+            gallery_items = JSON.parse(document.getElementById('gallery_' + itemID).dataset.json);
             options.index = parseInt(event.target.dataset.imageId, 10);
-            console.log(options);
-            let gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, gallery_items, options);
+            var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, gallery_items, options);
             document.getElementById(windowID).close();
             gallery.init();
             slideDownloadButton.slideURL = gallery.currItem.src;
 
-            gallery.listen('close', () => {
+            gallery.listen('close', function () {
                 if (document.documentElement.clientWidth > 1024) {
                     document.getElementById(windowID).showModal();
                 } else {
@@ -33,10 +32,9 @@ function bind_gallery() {
                 }
             });
 
-            gallery.listen('afterChange', () => {
+            gallery.listen('afterChange', function () {
                 slideDownloadButton.slideURL = gallery.currItem.src;
             });
         }
     });
-
 }
