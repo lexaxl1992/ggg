96bc4bd093dd5838622c1c9ccd4a22b03154bfaf
Martin Lawrence <marsixsoft@yahoo.com>
2017-10-22 06:37:06 +0300

    Big refactoring, fixed bugs

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,84 +1,87 @@
 class CategoriesController < ApplicationController
-	def show
-		@category = Category[params[:id]]
-	end
+  def show
+    @category = Category[params[:id]]
+  end
 
-	def new
-		respond_to do |format|
-			format.js
-		end
-	end
+  def new
+    respond_to do |format|
+      format.js
+    end
+  end
 
-	def edit
-		@category = Category[params[:id]]
-		respond_to do |format|
-			format.js
-		end
-	end
+  def edit
+    @category = Category[params[:id]]
+    respond_to do |format|
+      format.js
+    end
+  end
 
-	def create
-		# Сгенерировать индекс категории и добавить его в хэш параметров
-		lambda = params[:category].merge({"index": Digest::CRC32.hexdigest(params[:category][:label])})
+  def create
+    lambda = params[:category].merge(
+      "index": Digest::CRC32.hexdigest(params[:category][:label])
+    )
 
-		# Если был передан файл иконки,
-		unless lambda[:icon].nil?
-			category_params = lambda
-			uploaded_file = category_params[:icon]
-	    File.open(Rails.root.join('public', 'uploads', 'categories', category_params[:index]+".png"), 'wb') do |file|
-	    	# то записать её на диск по заданному пути
-	      file.write(uploaded_file.read)
-	    end
-	    # и записать её относительный путь для url в хэш
-			lambda = category_params.merge({"icon": "/uploads/categories/"+category_params[:index]+".png"})
-		end
+    unless lambda[:icon].nil?
+      props = lambda
+      uploaded_file = props[:icon]
+      File.open(Rails.root.join('public', 'uploads', 'categories', props[:index] + '.png'),'wb') do |file|
+        file.write(uploaded_file.read)
+      end
+      lambda = props.merge(
+        "icon": '/uploads/categories /' + props[:index] + '.png'
+      )
+    end
 
-		@category = Category.create(lambda)
+    @category = Category.create(lambda)
 
-		respond_to do |format|
-			if @category.save
-				format.js
-			else
-				format.js {render "error"}
-			end
-		end
-	end
+    respond_to do |format|
+      if @category.save
+        format.js
+      else
+        format.js { render 'error' }
+      end
+    end
+  end
 
-	def update
-		@category = Category[params[:categoryId]]
-		lambda = params[:category]
+  def update
+    @category = Category[params[:categoryId]]
+    lambda = params[:category]
 
-		# Если был передан файл иконки,
-		unless lambda[:icon].nil?
-			category_params = lambda
-			uploaded_file = category_params[:icon]
-	    File.open(Rails.root.join('public', 'uploads', 'categories', @category.index+".png"), 'wb') do |file|
-	    	# то записать её на диск по заданному пути
-	      file.write(uploaded_file.read)
-	    end
-	    # и записать её относительный путь для url в хэш
-			lambda = category_params.merge({"icon": "/uploads/categories/"+@category.index+".png"})
-		end
+    unless lambda[:icon].nil?
+      props = lambda
+      uploaded_file = props[:icon]
+      File.open(Rails.root.join('public', 'uploads', 'categories', @category.index + '.png'), 'wb') do |file|
+        file.write(uploaded_file.read)
+      end
+      lambda = props.merge(
+        "icon": '/uploads/categories/' + @category.index + '.png'
+      )
+    end
 
-		@category.update(lambda)
-		respond_to do |format|
-			if @category.save
-				format.js
-			else
-				format.js {render "error"}
-			end
-		end
-	end
+    @category.update(lambda)
+    respond_to do |format|
+      if @category.save
+        format.js
+      else
+        format.js { render 'error' }
+      end
+    end
+  end
 
-	def destroy
-		@category = Category[params[:id]]
-		@category_index = @category.index
-		@category.delete
-		respond_to do |format|
-			if @category.delete
-				format.js
-			else
-				format.js {render "error"}
-			end
-		end
-	end
+  def destroy
+    @category = Category[params[:id]]
+    @category_index = @category.index
+    @category.delete
+    respond_to do |format|
+      if @category.delete
+        format.js
+      else
+        format.js { render 'error' }
+      end
+    end
+  end
+
+  def update_order
+    sd
+  end
 end
