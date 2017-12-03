ffc10b3fb696fdac5c29046a8e01c20a03b5057c
Alina Bulgakova <yarabazarova@gmail.com>
2017-12-03 13:04:09 +0300

    updated manifest.js, updated footer

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,4 +2,4 @@
 //= link_directory ../javascripts .js
 //= link_directory ../../../vendor/assets/javascripts .js
 //= link_directory ../stylesheets .css
-//= link_directory ../../../vendor/assets/stylesheets .css
+//  link_directory ../../../vendor/assets/stylesheets .css
